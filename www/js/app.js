5dfa845f47f760dbcff35eb70094bb6067903da4
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2015-04-30 15:49:34 +0100

    Side content and center content are created.

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,6 @@
 angular.module('calorific', ['ionic'])
 
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -12,3 +13,35 @@ angular.module('calorific', ['ionic'])
     }
   });
 })
+
+
+.controller('CalorificCtrl', function($scope, $ionicModal){
+            $scope.tasks = [];
+    
+    // create and load the modal
+    $ionicModal.fromTemplateUrl('new-product.html', function(modal) {
+        $scope.productModal = modal;
+    },{
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+    
+    // called when the form is submited
+    $scope.createTask = function(product) {
+        $scope.products.push({
+            title: product.title
+        });
+        $scope.productModal.hide();
+        product.title = "";
+    };
+    
+    // Open our new product modal
+    $scope.newProduct = function() {
+        $scope.productModal.show();
+    };
+    
+    // Close the new product modal
+    $scope.closeNewProduct = function() {
+        $scope.productModal.hide();
+    };
+});
